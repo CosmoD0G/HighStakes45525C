cf07719ede023e41ff10b1f0e390d248756ea4b9
CosmoD0G <acneff27@gmail.com>
2024-09-27 08:19:15 -0400

    auton interface work

--- src/main.cpp
+++ src/main.cpp
@@ -7,19 +7,77 @@
 /*                                                                            */
 /*----------------------------------------------------------------------------*/
 #include "vex.h"
+#include <string>
 
 using namespace vex;
-
-// A global instance of vex::brain used for printing to the V5 brain screen
-vex::brain       Brain;
+using namespace std;
+// definition of all devices
+brain Brain;
+controller Controller1;
 
 // define your global instances of motors and other devices here
+
+enum PrintType {
+    COLOR,
+    SIDE,
+    PROGRAM
+};
+
+bool AUTON_TESTING_MODE = false;
+
+// prints the auton interface buttons to the screen
+void paintRectangles(PrintType mode) {
+    switch (mode) {
+        case COLOR:
+            Brain.Screen.setFillColor(red);
+            Brain.Screen.drawRectangle(3,50,200,75);
+            Brain.Screen.setFillColor(blue);
+            Brain.Screen.drawRectangle(240,50,200,75);
+            Brain.Screen.setFillColor(white);
+            Brain.Screen.drawRectangle(3,165,200,75);
+            break;
+        case SIDE:
+            Brain.Screen.setFillColor(green);
+            Brain.Screen.drawRectangle(3,50,200,75);
+            Brain.Screen.setFillColor(yellow);
+            Brain.Screen.drawRectangle(240,50,200,75); 
+            break; 
+        case PROGRAM:
+            Brain.Screen.setFillColor(orange);
+            Brain.Screen.drawRectangle(3,50,200,75);
+            Brain.Screen.setFillColor(purple);
+            Brain.Screen.drawRectangle(240,50,200,75);
+            Brain.Screen.setFillColor(white);
+            Brain.Screen.drawRectangle(3,165,200,75);
+            Brain.Screen.setFillColor(cyan);
+            Brain.Screen.drawRectangle(240,165,200,75);
+            break;
+    }
+}
+
+bool isTouchWithinRange() {
+    return false;
+}
+
+
+
+
+
 
 
 int main() {
 
-    Brain.Screen.printAt( 10, 50, "Hello V5" );
-   
+    paintRectangles(COLOR);
+
+    Brain.Screen.clearLine(0);
+    Brain.Screen.setFillColor(transparent);
+    Brain.Screen.print("select a color");
+
+
+
+
+
+
     while(1) {
         
         // Allow other tasks to run
